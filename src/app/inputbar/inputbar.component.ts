2d1c2ab3dd8bb73d564a55c4af9f3ded8b756fc0
jarvisar <jarvisar@mail.uc.edu>
2022-11-14 00:49:29 -0500

    Final push before work tomorrow

--- src/app/inputbar/inputbar.component.ts
+++ src/app/inputbar/inputbar.component.ts
@@ -15,38 +15,83 @@ export class InputbarComponent implements OnInit {
   yearData: any[] = ["Discovery Year", ];
   facilityData: any[] = ["Discovery Facility", ];
   
-  public selectedHost!: string;
-  public selectedMethod!: string;
-  public selectedYear!: string;
-  public selectedFacility!: string;
+  public selectedHostValue!: string;
+  public selectedMethodValue!: string;
+  public selectedYearValue!: string;
+  public selectedFacilityValue!: string;
 
   constructor(private papa: Papa, private data: DataService, private http: HttpClient) { }
 
-  private _selectedIndex!: number;
+  // initiate hostname select box
+  private _selectedHost!: number;
 
-  public get selectedIndex(): number {
-    return this._selectedIndex;
+  public get selectedHost(): number {
+    return this._selectedHost;
   }
 
-  public set selectedIndex(index: number) {
-    this._selectedIndex = index;
+  public set selectedHost(index: number) {
+    this._selectedHost = index;
+    this.selectedHostValue = this.hostData[index];
+    
+    console.log(this.selectedHostValue);
 
-    // Here you can set variable you would usualy set with the ngModel,
-    // using the provided index and the array I gues, where your names,
-    // addresses and social numbers are stored
-    // e.g.: this.selectedName = this.names[index];
-    this.selectedHost = this.hostData[index];
-    console.log(this.selectedHost);
+  }
+
+  // initiate discovery method select box
+  private _selectedMethod!: number;
+
+  public get selectedMethod(): number {
+    return this._selectedMethod;
+  }
+
+  public set selectedMethod(index: number) {
+    this._selectedMethod = index;
+    this.selectedMethodValue = this.methodData[index];
+    
+    console.log(this.selectedMethodValue);
+  }
+
+  // initiate discovery year select box
+  private _selectedYear!: number;
+
+  public get selectedYear(): number {
+    return this._selectedYear;
+  }
+
+  public set selectedYear(index: number) {
+    this._selectedYear = index;
+    this.selectedYearValue = this.yearData[index];
+    
+    console.log(this.selectedYearValue);
+  }
+
+  // initiate discovery facility select box
+  private _selectedFacility!: number;
+
+  public get selectedFacility(): number {
+    return this._selectedFacility;
+  }
+
+  public set selectedFacility(index: number) {
+    this._selectedFacility = index;
+    this.selectedFacilityValue = this.facilityData[index];
+    
+    console.log(this.selectedFacilityValue);
   }
 
   ngOnInit(): void {
     this.getHostList();
-    this.selectedIndex = 0;
-    console.log("test1");
+    this.getMethodList();
+    this.getYearList();
+    this.getFacilityList(); 
+    this.selectedHost = 0;
+    this.selectedMethod = 0;
+    this.selectedYear = 0;
+    this.selectedFacility = 0;
   }
 
   public getHostList() {
-    this.data.getHostList().subscribe(data => {
+    this.http.get('../assets/hostnames.csv', {responseType: 'text'}).subscribe(data => {
       data.split('\n').forEach(e => {
         e = e.replace(/['"]+/g, '');
         this.hostData.push(e);
@@ -56,11 +101,32 @@ export class InputbarComponent implements OnInit {
     console.log("test2");
   }
 
+  public getMethodList() {
+    this.http.get('../assets/discoverymethod.csv', {responseType: 'text'}).subscribe(data => {
+      data.split('\n').forEach(e => {
+        e = e.replace(/['"]+/g, '');
+        this.methodData.push(e);
+        console.log("test!")
+      })
+    })
+  }
+
+  public getYearList() {
+    this.http.get('../assets/disc_year.csv', {responseType: 'text'}).subscribe(data => {
+      data.split('\n').forEach(e => {
+        e = e.replace(/['"]+/g, '');
+        this.yearData.push(e);
+        console.log("test!")
+      })
+    })
+  }
+
   public getFacilityList() {
     this.http.get('../assets/disc_facility.csv', {responseType: 'text'}).subscribe(data => {
       data.split('\n').forEach(e => {
         e = e.replace(/['"]+/g, '');
         this.facilityData.push(e);
+        console.log("test!")
       })
     })
   }
