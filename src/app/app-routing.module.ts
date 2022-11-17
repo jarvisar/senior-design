fb7e198ac688f0604ae65340b65330afd44f5a70
jarvisar <jarvisar@mail.uc.edu>
2022-11-17 10:48:31 -0500

    Test lazy loading to speed up performance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inputbar',
+    loadChildren: () => import('./inputbar/inputbar.component').then(m => m.InputbarComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
