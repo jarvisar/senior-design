73c6b6ddca9bd87322089f44c2221ee1bdbb4839
jarvisar <jarvisar@mail.uc.edu>
2022-11-17 22:29:31 -0500

    Started creating table

--- src/app/exoplanet/exoplanet.component.ts
+++ src/app/exoplanet/exoplanet.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { DataService } from '../data.service';
 import { ChangeDetectionStrategy } from '@angular/core';
 import { LoadingService } from '../loading.service';
+import { Exoplanet } from './exoplanet'
 
 @Component({
   selector: 'app-exoplanet',
@@ -11,6 +12,8 @@ import { LoadingService } from '../loading.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExoplanetComponent implements OnInit {
+  
+  public exoplanetData!: Exoplanet[];
   
   constructor(private data: DataService, private http: HttpClient, public loadingService: LoadingService) { }
 
@@ -22,9 +25,11 @@ export class ExoplanetComponent implements OnInit {
   getExoplanetData(input: string){
     this.apiQuery = input;
     console.log(this.apiQuery);
-    this.data.getExoPlanetData(input).subscribe(data => {
-      console.log(data);
+    this.data.getExoPlanetData(input).subscribe((response: Exoplanet) => {
+      
     })
+
+    
   }
 
 }
