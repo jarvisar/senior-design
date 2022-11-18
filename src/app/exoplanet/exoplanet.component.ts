412571b4dab3811b3029a73543d0703d23f45f02
jarvisar <jarvisar@mail.uc.edu>
2022-11-17 21:42:36 -0500

    Animated loading spinner

--- src/app/exoplanet/exoplanet.component.ts
+++ src/app/exoplanet/exoplanet.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from '../data.service';
-import { Subject } from 'rxjs';
 import { ChangeDetectionStrategy } from '@angular/core';
 import { LoadingService } from '../loading.service';
 
@@ -9,7 +8,7 @@ import { LoadingService } from '../loading.service';
   selector: 'app-exoplanet',
   templateUrl: './exoplanet.component.html',
   styleUrls: ['./exoplanet.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExoplanetComponent implements OnInit {
   
