5b4de04c9acd86c9bdc36aeb55b0b567c1ad9562
jarvisar <jarvisar@mail.uc.edu>
2022-11-19 03:12:42 -0500

    Tried to fix CORS issue

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Subject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Exoplanet } from './exoplanet/exoplanet'
+
+const httpOptions = {
+  headers: new HttpHeaders({ 
+    'Access-Control-Allow-Origin':'*'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class DataService {
   hostUrl = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query='
   public exoplanetData: Array<Exoplanet> = [{pl_name: '11 Com b', hostname: '11 Com', discoverymethod: 'Radial Velocity', disc_year: 2007, disc_facility: 'Xinglong Station'}];
   
+  
   constructor(private http: HttpClient) {}
   
   getHostList(){
-    return this.http.get(this.hostUrl + 'select+distinct+hostname+from+pscomppars+order+by+hostname&format=csv', {responseType: 'text'});
+    return this.http.get(this.hostUrl + 'select+distinct+hostname+from+pscomppars+order+by+hostname&format=csv', httpOptions);
   }
 
   getExoPlanetData(query: string){
