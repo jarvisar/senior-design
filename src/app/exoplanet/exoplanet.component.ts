51fde3a56c29681f3b5a67f3a904723d4ab21c89
jarvisar <jarvisar@mail.uc.edu>
2022-11-18 21:11:20 -0500

    Cleaned up exoplanet component

--- src/app/exoplanet/exoplanet.component.ts
+++ src/app/exoplanet/exoplanet.component.ts
@@ -1,50 +1,33 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
-import { ChangeDetectionStrategy, ChangeDetectorRef  } from '@angular/core';
 
 import { Exoplanet } from './exoplanet'
-
-
-import { BehaviorSubject } from 'rxjs';
-
-
 
 @Component({
   selector: 'app-exoplanet',
   templateUrl: './exoplanet.component.html',
   styleUrls: ['./exoplanet.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExoplanetComponent implements OnInit { 
   public exoplanetData!: Array<any>;
-  
-  constructor(private data: DataService, private http: HttpClient, private cdr: ChangeDetectorRef) { }
+
+  constructor(private data: DataService) { }
   
   ngOnInit(): void {
-    
   }
 
-  public apiQuery!: string;
-  
   getExoplanetData(input: string){
-    var testArray: Array<Exoplanet> = [];
-    this.apiQuery = input;
-    this.data.getExoPlanetData(this.apiQuery).subscribe((response: any[]) => {
+    //Create blank array for new search
+    var newArray: Array<Exoplanet> = [];
+
+    this.data.getExoPlanetData(input).subscribe((response: any[]) => {
       response.forEach((e: Exoplanet) => {
-        
-        testArray.push(e)
-        
+        //Add each exoplanet to array
+        newArray.push(e)
       })
     });
-    console.log(this.exoplanetData);
-    this.exoplanetData = testArray;
-    console.log(this.exoplanetData);
+
+    this.exoplanetData = newArray;
     return this.exoplanetData;
   }
-
-  public trackData (index: number) {
-    return this.exoplanetData ? this.exoplanetData : undefined;
-  }
-
 }
