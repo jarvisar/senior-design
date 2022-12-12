619c98198ad01887432a3e0177c43b394ce042b6
jarvisar <jarvisar@mail.uc.edu>
2022-12-12 15:49:18 -0500

    Fixed an issue causing rows to expand after sortin

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -19,9 +19,10 @@ export const fadeInOut = (name = 'fadeInOut', duration = 0.1) =>
   animations: [
     fadeInOut('fadeinandout', 0.1),
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('collapsed, void', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
       state('expanded', style({ height: '*', visibility: 'visible' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ]
 })
