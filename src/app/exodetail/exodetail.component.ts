8e6856850b53f01e83eb712a5ece7a7c3a8eb2f9
jarvisar <jarvisar@mail.uc.edu>
2022-12-13 16:38:32 -0500

    Trying new things

--- src/app/exodetail/exodetail.component.ts
+++ src/app/exodetail/exodetail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Exoplanet } from '../exoplanet/exoplanet';
 
 @Component({
   selector: 'app-exodetail',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExodetailComponent implements OnInit {
 
+  @Input() exoplanet!: Exoplanet;
+
+  pl_name = this.exoplanet.pl_name;
+  hostname = this.exoplanet.hostname;
+  discoverymethod = this.exoplanet.discoverymethod;
+  disc_facility = this.exoplanet.disc_facility;
+ 
+  public formattedDiscFacility;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.formattedDiscFacility = this.disc_facility?.replace(/ /g, "_");
+    
   }
 
 }
