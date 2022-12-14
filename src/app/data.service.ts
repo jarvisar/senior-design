46aa038a02197cad4ec2731a866dc58e46a7be50
jarvisar <jarvisar@mail.uc.edu>
2022-12-14 17:58:37 -0500

    CORS

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -15,8 +15,8 @@ const httpOptions = {
 export class DataService {
   //As of right now API uses CORS.SH proxy to prevent CORS errors when returning data
   //Currently investigating better solutions such as running a NODE JS proxy server myself
-  //hostUrl = 'https://proxy.cors.sh/https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query='
-  hostUrl = '/TAP/sync?query='
+  hostUrl = 'https://proxy.cors.sh/https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query='
+  //hostUrl = '/TAP/sync?query='
   public exoplanetData: Array<Exoplanet> = [{pl_name: '11 Com b', hostname: '11 Com', discoverymethod: 'Radial Velocity', disc_year: 2007, disc_facility: 'Xinglong Station'}];
   
   constructor(private http: HttpClient) {}
