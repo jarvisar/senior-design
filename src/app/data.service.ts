4f8e009fde87c9966d2723486da72205020f6678
jarvisar <jarvisar@mail.uc.edu>
2023-01-12 16:07:44 -0500

    Configured CORS proxy

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Exoplanet } from './exoplanet/exoplanet'
 
 const headerDict = {
-  'Target-URL': 'https://exoplanetarchive.ipac.caltech.edu',
+  'Target-URL': 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync',
 }
 
 const httpOptions = {
@@ -15,10 +15,8 @@ const httpOptions = {
 })
 
 export class DataService {
-  //As of right now API uses CORS.SH proxy to prevent CORS errors when returning data
-  //Currently investigating better solutions such as running a NODE JS proxy server myself
-  hostUrl = 'https://cors-proxy-phi.vercel.app/proxy/TAP/sync?query='
-  //hostUrl = '/TAP/sync?query='
+  // Use the CORS proxy to set CORS headers
+  hostUrl = 'https://cors-proxy-phi.vercel.app/proxy?query='
   public exoplanetData: Array<Exoplanet> = [{pl_name: '11 Com b', hostname: '11 Com', discoverymethod: 'Radial Velocity', disc_year: 2007, disc_facility: 'Xinglong Station'}];
   
   constructor(private http: HttpClient) {}
