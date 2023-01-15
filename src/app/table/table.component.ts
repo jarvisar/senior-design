3923ed154abc2838ab7921219159a36347eca38e
jarvisar <jarvisar@mail.uc.edu>
2023-01-15 13:05:02 -0500

    Fixed animation

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -24,7 +24,7 @@ export const fadeInOut = (name = 'fadeInOut', duration = 0.1) =>
       state('collapsed, void', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
       state('expanded', style({ height: '*', visibility: 'visible' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-      transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
     ]),
   ]
 })
