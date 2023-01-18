bda42b5ac5c090a75900d623278a5beb22346639
jarvisar <jarvisar@mail.uc.edu>
2023-01-18 01:05:21 -0500

    Added star wars planets

--- src/app/secret/secret.component.ts
+++ src/app/secret/secret.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Planet {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string;
+}
 
 @Component({
   selector: 'app-secret',
@@ -10,24 +23,64 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class SecretComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private http: HttpClient) { }
 
   // open secret da]ialog box
   public openDialog() {
-    const dialogRef = this.dialog.open(SecretComponent, {
-      panelClass: 'mat-dialog-container',
-    });
+    let dialogConfig = new MatDialogConfig();
+    dialogConfig.minWidth = '50%';
+    dialogConfig.minHeight = '80%';
+    const dialogRef = this.dialog.open(SecretComponent, dialogConfig);
   }
 
-  locations = ["tatooine","hoth","jakku","geonosis","bespin","naboo","crait","coruscant","alderaan"];
-  displayInfo = false;
-  planetImageUrl = 'https://starloggers.files.wordpress.com/2020/01/';
+  planets: Planet[] = [];
+  displayedColumns: string[] = ['name', 'rotation_period', 'orbital_period', 'diameter', 'climate', 'gravity', 'terrain', 'surface_water', 'population'];
+  tableDef: any = {
+    name: {
+      header: 'Name',
+      type: 'string'
+    },
+    rotation_period: {
+      header: 'Rotation Period',
+      type: 'string'
+    },
+    orbital_period: {
+      header: 'Orbital Period',
+      type: 'string'
+    },
+    diameter: {
+      header: 'Diameter',
+      type: 'string'
+    },
+    climate: {
+      header: 'Climate',
+      type: 'string'
+    },
+    gravity: {
+      header: 'Gravity',
+      type: 'string'
+    },
+    terrain: {
+      header: 'Terrain',
+      type: 'string'
+    },
+    surface_water: {
+      header: 'Surface Water',
+      type: 'string'
+    },
+    population: {
+      header: 'Population',
+      type: 'string'
+    }
+  };
 
-  color(){
-    return "hsl("+Math.floor(Math.random()*360)+"deg,100%,40%)"
+
+  ngOnInit() {
+    this.http.get('https://swapi.dev/api/planets/')
+      .subscribe((response: any) => {
+        this.planets = response.results;
+      });
   }
-  
-  ngOnInit(): void {}
 
   
 }
