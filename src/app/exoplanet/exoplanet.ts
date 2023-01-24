d5d79292a8ac7384a00d9e5c0eb4e127b0db7dfe
jarvisar <jarvisar@mail.uc.edu>
2023-01-23 21:02:24 -0500

    Added system composition columns

--- src/app/exoplanet/exoplanet.ts
+++ src/app/exoplanet/exoplanet.ts
@@ -1,8 +1,13 @@
 export interface Exoplanet{
-    pl_name?: string;
-    hostname?: string;
-    discoverymethod?: string;
-    disc_year?: number;
-    disc_facility?: string;
-    disc_refname?: HTMLElement;
+    pl_name?: string; // Exoplanet name
+    hostname?: string; // Host name (star name)
+    discoverymethod?: string; // Discovery method
+    disc_year?: number; // Discovery year
+    disc_facility?: string; // Discovery facility
+    disc_refname?: HTMLElement; // Anchor HTML element to discovery publication
+    pl_controv_flag?: number; // 1 = controversial, 0 = non-controversial
+    sy_snum?: number; // Number of stars in system
+    sy_pnum?: number; // Number of planets in system
+    sy_mnum?: number; // Number of moons in system
+    cb_flag?: number; // Indicates if exoplanet orbits binary system: 1 = yes, 0 = no
 }
