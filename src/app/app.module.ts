241b25b9c1495636d9f4bbfe5a2b31c6e3a1826c
jarvisar <jarvisar@mail.uc.edu>
2023-01-27 00:28:44 -0500

    Service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -70,7 +70,13 @@ import { environment } from '../environments/environment';
     MatPaginatorModule,
     CdkTableModule,
     ScrollingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }) 
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }) 
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
