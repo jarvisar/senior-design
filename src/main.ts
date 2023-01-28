e4cf7dfefcf1ced8fcfd8bdb993b0e95e5b6635f
jarvisar <jarvisar@mail.uc.edu>
2023-01-28 17:32:03 -0500

    Added service worker to main

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,7 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  if ('serviceWorker' in navigator && environment.production) {
+    navigator.serviceWorker.register('/ngsw-worker.js');
+  }
