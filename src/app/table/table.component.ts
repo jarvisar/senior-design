c2137d56349dbd9ebc39c59fea5695371d0a773d
jarvisar <jarvisar@mail.uc.edu>
2023-02-04 21:51:38 -0500

    Changed animation timing

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -30,7 +30,7 @@ export const fadeInOut = (name = 'fadeInOut', duration = 0.2) =>
       state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
       state('void', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
       state('expanded', style({ height: '*', visibility: 'visible' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> collapsed', animate('400ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
       transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
     ]),
     trigger('blur', [
