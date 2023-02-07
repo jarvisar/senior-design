fe586b9206deb1f7bf5fac23c5e51733a1ba3997
jarvisar <jarvisar@mail.uc.edu>
2023-02-06 23:28:03 -0500

    Added default columns to column service

--- src/app/columns.service.ts
+++ src/app/columns.service.ts
@@ -6,6 +6,9 @@ import { Injectable } from '@angular/core';
 export class ColumnsService {
 
   // Define default columns
-  public displayedColumns = ['index', 'pl_name', 'hostname', 'discoverymethod', 'disc_year', 'pl_rade', 'pl_bmasse', 'pl_dens', 'disc_facility'];
-  constructor() { }
+  public displayedColumns;
+  public defaultColumns = ['index', 'pl_name', 'hostname', 'discoverymethod', 'disc_year', 'pl_rade', 'pl_bmasse', 'pl_dens', 'disc_facility'];
+  constructor() {
+    this.displayedColumns = this.defaultColumns;
+  }
 }
