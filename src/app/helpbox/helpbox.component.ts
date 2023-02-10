5ba1b8765105d1e3886698d37029c19804a2ef7f
jarvisar <jarvisar@mail.uc.edu>
2023-02-10 00:57:50 -0500

    Added clear cache button

--- src/app/helpbox/helpbox.component.ts
+++ src/app/helpbox/helpbox.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-helpbox',
@@ -9,11 +10,16 @@ import { MatDialog } from '@angular/material/dialog';
 @Injectable()
 export class HelpboxComponent implements OnInit {
   
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private data: DataService) { }
 
   // Opens help box window
   public openDialog() {
     const dialogRef = this.dialog.open(HelpboxComponent);
+  }
+
+  clearData(){
+    localStorage.clear();
+    this.data.getAllExoplanetData();
   }
 
   ngOnInit(): void {
