7bb9f7e449f6fed3833ebc26e9cb035196094d36
jarvisar <jarvisar@mail.uc.edu>
2023-02-18 20:53:26 -0500

    Formatting

--- src/app/settings-dialog/settings-dialog.component.ts
+++ src/app/settings-dialog/settings-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Injectable, Input, Inject } from '@angular/core';
+import { Component, OnInit, Injectable, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ColumnsService } from '../columns.service';
 
